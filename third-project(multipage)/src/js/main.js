43f7948ab3125fe9319c6f10c4197a8bff83c822
Antay Juskovets <31548180+VeselAbaya@users.noreply.github.com>
2018-07-22 18:40:08 +0300

    Console.log's deleted

--- third-project(multipage)/src/js/main.js
+++ third-project(multipage)/src/js/main.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
   let windowSize = $(window).height();
-  console.log($('body').height())
-  console.log(windowSize)
   if ($('body').height() < windowSize) {
     $('footer').css({
       'position': 'absolute',
