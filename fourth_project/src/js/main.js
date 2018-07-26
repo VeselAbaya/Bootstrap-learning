2147f37f4b8f6019a0deaac77f75a76f367dbb72
VeselAbaya <Vandervise465@gmail.com>
2018-07-26 17:28:58 +0300

    index.html is ready

---
+++ fourth_project/src/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('.video').click(function () {
+    var modal = $(this).data('target'),
+    videoSRC = $(this).attr('data-video'),
+    videoSRCauto = videoSRC + '?autoplay=1';
+    $(modal + ' iframe').attr('src', videoSRCauto);
+    $(modal + ' button.close').click(function () {
+      $(modal + ' iframe').attr('src', videoSRC);
+    });
+
+    $(modal).click(function () {
+      $(modal + ' iframe').attr('src', videoSRC);
+    });
+  });
+})
